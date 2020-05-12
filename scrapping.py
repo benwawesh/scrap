b7318a72ffa9a9678c2929faf552633e630a636b
benwawesh <benjojo47@gmail.com>
2020-05-12 10:41:40 +0300

    using requests instead of urllib.request

--- scrapping.py
+++ scrapping.py
@@ -1,11 +1,22 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import  requests
 my_url ="https://www.tenders.go.ke/website/tenders/Index"
-uclient=uReq(my_url)
-page_html= uclient.read()
-uclient.close()
-page_soup=soup(page_html, "html.parser")
+uclient=requests.get(my_url)
+page_soup=soup(uclient.text, "html.parser")
 print(page_soup.table)
-containers=page_soup.findAll('div',{"class":"item-container"})
-print(len(containers))
+tender_table=page_soup.findAll('tbody',{'class':'odd'})
+print(tender_table)
+
+
+# response=requests.get(my_url)
+# soup= soup(response.text, 'html.parser')
+# print(soup.table)
+# tender_table = soup.find('table', {"class":"table table--football table--league-table table--responsive-font table--striped"}).tbody
+# print(tender_table)
+# for tender in tender_table.find_all('tbody'):
+#     rows=tender.find_all('tr')
+#     for row in rows:
+#         tender_objects = row.find('td', class_="sorting_1")
+#         print(tender_objects)
